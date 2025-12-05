c4f8f2f9e9d6b7c579641a452208b6db989b93f6
man-in-dev <man.in.dev.16@gmail.com>
2025-12-05 23:53:00 +0530

    bug/fix

--- config/database.js
+++ config/database.js
@@ -1,6 +1,8 @@
 import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
 
-const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/makario';
+const MONGODB_URI = process.env.MONGODB_URI;
 
 let isConnected = false;
 
